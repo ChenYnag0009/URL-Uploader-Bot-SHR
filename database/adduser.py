c76c7494f55267b9d1470e114e8e6b693ccfd761
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-19 19:54:24 +0700

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -12,7 +12,6 @@ ID - <code>{}</code>
 Name - {}  
 Username - @{}  
 User Link - {}  
-Forward Video - {}"""
 
 async def AddUser(bot: Client, update: Message):
     if not await techvj.is_user_exist(update.from_user.id):
@@ -21,22 +20,3 @@ async def AddUser(bot: Client, update: Message):
         log_info = LOG_TEXT_P.format(update.from_user.id, update.from_user.mention)
         log_info += "\nUsername: @" + (update.from_user.username if update.from_user.username else "N/A")
         log_info += "\nUser Link: " + update.from_user.mention
-        
-        await bot.send_message(Config.TECH_VJ_LOG_CHANNEL, log_info)
-
-    # Forward message to log channel
-    log_message = await update.forward(Config.TECH_VJ_LOG_CHANNEL)
-    
-    # Create log info message
-    log_info = "Message Sender Information\n"
-    log_info += "\nFirst Name: " + update.from_user.first_name
-    log_info += "\nUser ID: " + str(update.from_user.id)
-    log_info += "\nUsername: @" + (update.from_user.username if update.from_user.username else "N/A")
-    log_info += "\nUser Link: " + update.from_user.mention
-
-    # Reply to the forwarded message with user info
-    await log_message.reply_text(
-        text=log_info,
-        disable_web_page_preview=True,
-        quote=True
-    )
