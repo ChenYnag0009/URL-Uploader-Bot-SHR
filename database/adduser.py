88e418b01b4ceb3ac286d40834586cbd6bfd81e5
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-19 20:04:18 +0700

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -17,10 +17,9 @@ async def AddUser(bot: Client, update: Message):
     if not await techvj.is_user_exist(update.from_user.id):
         await techvj.add_user(update.from_user.id)
         
-        # Prepare log information
         log_info = LOG_TEXT_P.format(update.from_user.id, update.from_user.mention)
         log_info += "\nUsername: @" + (update.from_user.username if update.from_user.username else "N/A")
         log_info += "\nUser Link: " + update.from_user.mention
         
         # Send log message to the log channel
-        await bot.send_message(Config.TECH_VJ_LOG_CHANNEL, log_info)
+        await bot.send_message(Config.TECH_VJ_LOG_CHANNEL, LOG_TEXT_P.format(update.from_user.id, update.from_user.mention))
